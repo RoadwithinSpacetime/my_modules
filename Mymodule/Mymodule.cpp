5cbff6ac2254c974b8cff54340b441083431cf27
RoadwithinSpacetime <roadwithinspacetime@gmail.com>
2025-08-13 00:21:09 +0200

    Mymodule sum
    added Mymodule added code formula

---
+++ Mymodule/Mymodule.cpp
@@ -0,0 +1,61 @@
+#include "mp_sdk_audio.h"
+
+using namespace gmpi;
+
+class Mymodule final : public MpBase2
+{
+	AudioInPin pinInput1;
+	AudioInPin pinInput2;
+	AudioOutPin pinOutput;
+
+public:
+	Mymodule()
+	{
+		initializePin( pinInput1 );
+		initializePin( pinInput2 );
+		initializePin( pinOutput );
+	}
+
+	void subProcess( int sampleFrames )
+	{
+		// get pointers to in/output buffers.
+		auto input1 = getBuffer(pinInput1);
+		auto input2 = getBuffer(pinInput2);
+		auto output = getBuffer(pinOutput);
+
+		for( int s = sampleFrames; s > 0; --s )
+		{
+			// TODO: Signal processing goes here.
+
+			*output = *input1 * *input2;
+
+
+			// Increment buffer pointers.
+			++input1;
+			++input2;
+			++output;
+		}
+	}
+
+	void onSetPins() override
+	{
+		// Check which pins are updated.
+		if( pinInput1.isStreaming() )
+		{
+		}
+		if( pinInput2.isStreaming() )
+		{
+		}
+
+		// Set state of output audio pins.
+		pinOutput.setStreaming(true);
+
+		// Set processing method.
+		setSubProcess(&Mymodule::subProcess);
+	}
+};
+
+namespace
+{
+	auto r = Register<Mymodule>::withId(L"My Mymodule");
+}
