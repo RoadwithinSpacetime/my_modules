64afc8a1dd84234cf4b7beb8070215be5dfa01cd
RoadwithinSpacetime <roadwithinspacetime@gmail.com>
2025-08-13 21:04:14 +0200

    RwS Multiply
    RwSMultiply added

---
+++ RwSMultiply/RwSMultiply.cpp
@@ -0,0 +1,58 @@
+#include "mp_sdk_audio.h"
+
+using namespace gmpi;
+
+class RwSMultiply final : public MpBase2
+{
+	AudioInPin pinInput1;
+	AudioInPin pinInput2;
+	AudioOutPin pinOutput;
+
+public:
+	RwSMultiply()
+	{
+		initializePin( pinInput1 );
+		initializePin( pinInput2 );
+		initializePin( pinOutput );
+	}
+
+	void subProcess( int sampleFrames )
+	{
+		// get pointers to in/output buffers.
+		auto input1 = getBuffer(pinInput1);
+		auto input2 = getBuffer(pinInput2);
+		auto output = getBuffer(pinOutput);
+
+		for( int s = sampleFrames; s > 0; --s )
+		{
+			// TODO: Signal processing goes here.
+
+			// Increment buffer pointers.
+			++input1;
+			++input2;
+			++output;
+		}
+	}
+
+	void onSetPins() override
+	{
+		// Check which pins are updated.
+		if( pinInput1.isStreaming() )
+		{
+		}
+		if( pinInput2.isStreaming() )
+		{
+		}
+
+		// Set state of output audio pins.
+		pinOutput.setStreaming(true);
+
+		// Set processing method.
+		setSubProcess(&RwSMultiply::subProcess);
+	}
+};
+
+namespace
+{
+	auto r = Register<RwSMultiply>::withId(L"My RwSMultiply");
+}
