ea2f6504f8f3fb9757620739257af31bb463fb9b
RoadwithinSpacetime <roadwithinspacetime@gmail.com>
2025-09-26 17:43:35 +0200

    CyclePeakLookahead

--- CyclePeakLookahead/CyclePeakLookahead.h
+++ CyclePeakLookahead/CyclePeakLookahead.h
@@ -43,7 +43,7 @@ private:
     // --- Quantisation (ceil/floor) ---
     bool  useCeil_ = true;   // enable upward rounding
     bool  useFloor_ = true;  // enable downward rounding
-    float quantStep_ = 0.1f; // step size (1 decimal, 0.1 V)
+    float quantStep_ = 1.0f; // step size (1 decimal, 0.1 V)
 
     // --- Misc ---
     double sampleRate_;
